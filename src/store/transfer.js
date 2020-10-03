94fdc53a9c19c87447996731a08f28a7a8e23e72
bagasafrz7 <bagasafrz16@gmail.com>
2020-10-03 21:55:54 +0700

    Add get data history transaction

--- src/store/transfer.js
+++ src/store/transfer.js
@@ -5,11 +5,22 @@ export default {
   state: {
     page: 1,
     limit: 3,
-    recentTransferHome: {}
+    search: '',
+    totalPage: '',
+    span: 'week',
+    recentTransferHome: {},
+    historyTransaction: {}
   },
   mutations: {
     setRecentTransferHome(state, payload) {
       state.recentTransferHome = payload
+    },
+    setHistoryTransaction(state, payload) {
+      state.historyTransaction = payload.data
+      state.totalPage = payload.pagination.totalData
+    },
+    changePage(state, payload) {
+      state.page = payload
     }
   },
   actions: {
@@ -25,11 +36,31 @@ export default {
             reject(error.response.data)
           })
       })
+    },
+    dataHistoryTransaction(context, payload) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`${process.env.VUE_APP_URL}transaction/history/?id=${payload.id}&span=${context.state.span}&page=${context.state.page}`)
+          .then(response => {
+            resolve(response.data)
+            context.commit('setHistoryTransaction', response.data)
+          })
+          .catch(error => {
+            reject(error.response)
+          })
+      })
     }
   },
   getters: {
     getRecentTransferHome(state) {
       return state.recentTransferHome
+    },
+    getHistoryTransaction(state) {
+      return state.historyTransaction
+    },
+    getTotalPage(state) {
+      // console.log(state.totalPage)
+      return state.totalPage
     }
   }
 }
